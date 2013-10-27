6764168c483d82b4933e8ea9abed973a63884b67
Clarkok <mail@clarkok.com>
2013-10-27 15:17:16 +0800

    Decorate

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ $(document).ready(function(){
 	var	num	= $('#nump');
 	var	rest	= -1;
 	var	total	= 0;
+	var	level	= 0;
+	var	str	= ['三等奖', '二等奖', '一等奖', '特等奖', '没有了'];
+	var	need_new= true;
 	$("#cnt").val(1);
 	$("#start").click(function(){
 		console.debug("started");
@@ -20,10 +23,11 @@ $(document).ready(function(){
 				if (!rest){
 					clearInterval(drawer);
 					drawer	= null;
+					$('#ans').prepend('<p>');
 					return;
 				}
 				if (total >= 216){
-					$('#ans').append("抽完了QAQ");
+					$('p#nump').html('抽完了QAQ');
 					clearInterval(drawer);
 					drawer	= null;
 					return;
@@ -46,11 +50,32 @@ $(document).ready(function(){
 			}, 80);
 			$("#start").html('停止');
 		} else {
+			if (need_new){
+				$('#anssheel').prepend('<div id="ans" class="answers"></div>');
+				need_new	= false;
+			}
 			rest	= $('#cnt').val();
+			$('#ans').prepend('</p>');
 			$("#start").html('开始');
 		}
 	});
-	$("#addhr").click(function(){
-		$("#ans").prepend("<hr>");
+	$('#level').html(str[level]);
+	$('#level').click(function(){
+		level++;
+		need_new	= true;
+		$('#ans').animate({'top':'-8em'});
+		$('#level').html(str[level]);
+		$('.answers').unbind("click").click(function(){
+			$(this).animate({'top':'0'});
+			$(this).attr('id', '#'+(level-1));
+		});
+		if (level <= 4){
+			$('#ans').prepend('<h2>'+str[level-1]+'</h2>');
+		}
+		else {
+			$('#num').animate({'opacity':'0'}, 'fast', function(){$('#num').hide();});
+			$('#anssheel *').stop().css({'top': '0px'});
+			$('#anssheel').animate({'top': '0'});
+		}
 	});
 });
